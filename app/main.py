0f216900ebadeb68f855342cf0b17ea928f99ac3
EcolMaster <cloudym444@gmail.com>
2022-04-03 21:05:51 +0500

    закончил делать окно авторизации

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
 from forms.user import LoginForm, RegisterForm
-
+from data import db_session
+from data.users import User
+from flask_login import login_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+db_session.global_init("db/users.db")
+db_sess = db_session.create_session()
 
 @app.route('/')
 def start_site():
@@ -14,8 +19,15 @@ def start_site():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/learn/<user_id:int>')
-    return render_template('login_page.html', title='Авторизация', form=form)
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/learn")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login_page.html', form=form)
 
 
 # начал делать форму для регистрации
