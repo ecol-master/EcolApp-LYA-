1d2bbb534525247ad3aee9c4ae4cdc21265990a0
EcolMaster <cloudym444@gmail.com>
2022-04-12 22:46:28 +0500

    change main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
-from flask import Flask, render_template, redirect
+import json
+import random
+
+from flask import Flask, redirect, render_template
+from flask_login import LoginManager, current_user, login_user
 from flask_wtf import FlaskForm
-from forms.user import LoginForm, RegisterForm
+from numpy import add, less
+
 from data import db_session
+from data.lessons import Lesson
 from data.users import User
-from flask_login import login_user, LoginManager
+from forms.user import LoginForm, RegisterForm
+from data.tests import Test
+from data.questions import Question
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -34,7 +41,7 @@ def login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             return redirect("/study")
-        return render_template('login.html',
+        return render_template('login_page.html',
                                message="Неправильный логин или пароль",
                                form=form)
     return render_template('login_page.html', form=form)
@@ -59,7 +66,7 @@ def reqister():
                                    form=form,
                                    message="Такой пользователь уже есть")
         user = User(
-            name=form.name.data,
+            nickname=form.name.data,
             email=form.email.data,
             description=form.about.data
         )
@@ -69,11 +76,86 @@ def reqister():
         return redirect('/study')
     return render_template('register_page.html', title='Регистрация', form=form)
 
+
+def add_lessons():
+    lessons = [
+        {"title":"Экология", 
+        "title_image":"../static/images/logo_welcome_page.svg",
+        "href":"../study/ecology" 
+        },
+        {"title":"Земля", 
+        "title_image":"../static/images/logo_welcome_page.svg",
+        "href":"../study/eath" 
+        },
+        {"title":"Вода", 
+        "title_image":"../static/images/logo_welcome_page.svg", 
+        "href":"../study/water"
+        },
+        {"title":"Небо", 
+        "title_image":"../static/images/logo_welcome_page.svg", 
+        "href":"../study/sky"
+        }
+    ]
+
+    
+    for lesson in lessons:
+        lesson_db = Lesson()
+        lesson_db.title = lesson["title"]
+        lesson_db.title_image = lesson["title_image"]
+        lesson_db.href = lesson["href"]
+        db_sess.add(lesson_db)
+        db_sess.commit()
+
+def add_questions():
+    file_question = open("templates/questions.json")
+    all_questions = json.load(file_question)
+    for quiz in all_questions["questions"]:
+        quiz_db = Question()
+        quiz_db.text = quiz["text"]
+        quiz_db.rigth_answer = quiz["answer"]
+        quiz_db.points = 5
+        db_sess.add(quiz_db)
+        db_sess.commit()
+
+
+# обработчик при переходе на страницу уроков
 @app.route('/study')
 def learn():
-    return render_template("study_page.html")
+#     user_id = current_user.id
+    add_lessons()
+    lessons = db_sess.query(Lesson).all()
+    return render_template("study_page.html", lessons=lessons)
 
-# some changes
+@app.route("/study/<lesson>")
+def show_lesson(lesson):
+    add_questions()
+    questions_for_test = random.choices([str(q.id) for q in db_sess.query(Question).all()], k=5)
+
+    new_test = Test()
+    new_test.questions = ' '.join(questions_for_test)
+    new_test.current_question = 0
+
+    try:
+        new_test.user_id = current_user.id
+
+        
+        db_sess.add(new_test)
+        db_sess.commit()
+
+        user = db_sess.query(User).filter(User.id == current_user.id).first()
+        user.now_test_id = new_test.id
+        db_sess.commit(user)
+    except Exception:   
+        db_sess.add(new_test)
+        db_sess.commit()
+
+    question = db_sess.query(Question).filter(Question.id == int(questions_for_test[0])).first()
+    return render_template("lesson_page.html", question=question)
+
+@app.route("/study/<lesson>/<int:num_question>")
+def show_new_question():
+    pass
+
 
 if __name__ == "__main__":
     app.run()
